3951cafc69477a6fc4ce181c380ff9deb1d56d09
Vincenzo Sagristano <vincenzo.sagristano@iongroup.com>
2024-05-18 10:05:52 +0200

    added: sidesheet component

---
+++ src/app/components/sidesheet/sidesheet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sidesheet',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidesheet.component.html',
+  styleUrl: './sidesheet.component.css'
+})
+export class SidesheetComponent {
+
+}
